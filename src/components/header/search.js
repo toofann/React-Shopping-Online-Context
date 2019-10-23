8d789671b067b6f16414df23eed181d2ffa03078
Toofan <toofan0831@gmail.com>
2019-10-23 21:14:23 +0330

    optiemized and performance

--- src/components/header/search.js
+++ src/components/header/search.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { useState } from "react";
 import { InputSearch } from "./uiHeader";
 
 const Search = () => {
--- src/components/newProduct/shoeNewItem.js
+++ src/components/newProduct/shoeNewItem.js
@@ -1,4 +1,4 @@
-import React, { memo } from "react";
+import React from "react";
 import { SectionShoeDataChild, LinkShoeData } from "./uiNewProduct";
 
 const ShoeNewItem = ({ data }) => {
